cedb41ec81974304c8c511e13092d6cc09d1f3ae
German M <trapavoided22@gmail.com>
2017-02-06 21:28:44 -0600

    9:28

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import forms.Principal;
+
+/**
+ *
+ * @author German
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Principal  llamar = new Principal();
+        llamar.setLocationRelativeTo(null);
+        llamar.setVisible(true);
+    }
+    
+}
